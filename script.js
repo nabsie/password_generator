67b8e68f061806631bbae3f18cfc7da96f7e0f32
nabsiek <nabsiek@gmail.com>
2021-03-13 19:59:09 +0000

    wip added boilerplate

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Declare a new function 
+function generatePassword() {
+	// Create a variable to store our generated password
+	var password = ''
 
+	// Logic would go here
+	...
+
+	// Return our created password
+	return password;
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
